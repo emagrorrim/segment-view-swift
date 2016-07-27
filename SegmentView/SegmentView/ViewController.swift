aa6ed3272a0bd0ba06027ae0cafa3b2b54ccd68e
xin-guo <xinguo@thoughtworks.com>
2016-07-27 23:04:17 +1000

    Add an example

--- SegmentView/SegmentView/ViewController.swift
+++ SegmentView/SegmentView/ViewController.swift
@@ -11,14 +11,20 @@ class ViewController: UIViewController, EMSegmentViewDelegate{
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    // Create with Interface Builder
     segmentView.delegate = self
     segmentView.configureSegmentTitle(["BUY", "RENT", "SOLD"])
     
-    let segView: EMSegmentView = EMSegmentView(segmentTitles: ["A", "B"])
+    //Create with code
+    let segView: EMSegmentView = EMSegmentView(segmentTitles: ["BUY", "RENT", "SOLD"])
     segView.delegate = self
     segView.configureSegmentView(UIColor.purpleColor())
-    segView.frame = CGRectMake(20, 20, 320, 35)
     view.addSubview(segView)
+    segView.translatesAutoresizingMaskIntoConstraints = false
+    self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("[segView(300)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["segView": segView]))
+    self.view.addConstraints(NSLayoutConstraint.constraintsWithVisualFormat("V:|-140-[segView(35)]", options: NSLayoutFormatOptions(rawValue: 0), metrics: nil, views: ["segView": segView]))
+    self.view.addConstraint(NSLayoutConstraint(item: segView, attribute: .CenterX, relatedBy: .Equal, toItem: self.view, attribute: .CenterX, multiplier: 1.0, constant: 0.0))
+    
   }
   
   override func didReceiveMemoryWarning() {
